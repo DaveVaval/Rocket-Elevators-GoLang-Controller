a19075da57f8e49189fe8fc7d7132c65d40a3009
DaveGrim <DaveGrim@users.noreply.github.com>
2021-02-10 09:25:39 -0500

    Struct attributes are done

--- main.go
+++ main.go
@@ -11,28 +11,52 @@ type Battery struct {
 	amountOfFloors          int
 	amountOfColumns         int
 	amountOfBasements       int
-	columnsList             []string
-	floorRequestButtonsList []string
+	columnsList             []Column
+	floorRequestButtonsList []FloorRequestButton
 }
 
 // Column struct
 type Column struct {
+	ID                int
+	status            string
+	amountOfFloors    int
+	amountOfElevators int
+	isBasement        bool
+	elevatorsList     []Elevator
+	callButtonsList   []CallButton
+	servedFloors      []int
 }
 
 // Elevator struct
 type Elevator struct {
+	ID               int
+	status           string
+	amountOfFloors   int
+	direction        nil
+	currentFloor     int
+	door             Door
+	floorRequestList []int
 }
 
 // CallButton struct
 type CallButton struct {
+	ID        int
+	status    string
+	floor     int
+	direction string
 }
 
 // FloorRequestButton struct
 type FloorRequestButton struct {
+	ID     int
+	status string
+	floor  int
 }
 
 // Door struct
 type Door struct {
+	ID     int
+	status string
 }
 
 func main() {
